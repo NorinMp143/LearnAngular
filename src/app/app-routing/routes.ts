a1b3d9916b2ff0a7414fd93b4c7d3d04973e3bcf
NorinMp143 <madanparmar637@gmail.com>
2020-07-18 11:03:15 +0530

    Single Page Application Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -9,5 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'menu',component:MenuComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
     {path:'', redirectTo:'/home',pathMatch:"full"}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -44,7 +45,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
