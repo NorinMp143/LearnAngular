3005cef21ad4a2343510504be4b8c21f8428ec9b
NorinMp143 <madanparmar637@gmail.com>
2020-07-23 13:22:51 +0530

    RxJs Part1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -19,7 +19,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getDishes()
-    .then((dishes) => this.dishes = dishes);
+    .subscribe((dishes) => this.dishes = dishes);
   }
 
   onSelect(dish: Dish){
